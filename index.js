fa1cc1657d60d0c2db02a24a5d1a5f2ef432c80f
LKNorman <lknorman30@gmail.com>
2020-04-07 22:11:15 -0400

    added basic index.js code

---
+++ index.js
@@ -0,0 +1,39 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const logo = require("asciiart-logo");
+const config = require("package.json");
+
+// connecting to the database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "employee_tracker_db",
+});
+
+// error function
+connection.connect(function (err) {
+  if (err) throw err;
+  runDB();
+});
+
+// function to ask what to do
+function runDB() {
+  inquirer.prompt({
+    name: "action",
+    type: "rawlist",
+    message: "Please select an action.",
+    choices: [
+      "Add a role",
+      "Add a department",
+      "Add an employee",
+      "Update an employee's role",
+      "View all roles",
+      "View all departments",
+      "View all employees",
+    ],
+  })
+  .then
+}
