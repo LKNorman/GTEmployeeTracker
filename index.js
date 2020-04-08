9530a30f4f80785d241490d28be3a4476483d232
LKNorman <lknorman30@gmail.com>
2020-04-07 22:41:37 -0400

    added role add function

--- index.js
+++ index.js
@@ -21,19 +21,81 @@ connection.connect(function (err) {
 
 // function to ask what to do
 function runDB() {
-  inquirer.prompt({
-    name: "action",
-    type: "rawlist",
-    message: "Please select an action.",
-    choices: [
-      "Add a role",
-      "Add a department",
-      "Add an employee",
-      "Update an employee's role",
-      "View all roles",
-      "View all departments",
-      "View all employees",
-    ],
-  })
-  .then
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "Please select an action.",
+      choices: [
+        "Add a role",
+        "Add a department",
+        "Add an employee",
+        "Update an employee's role",
+        "View all roles",
+        "View all departments",
+        "View all employees",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add a role":
+          addRole();
+          break;
+
+        case "Add a department":
+          addDepartment();
+          break;
+
+        case "Add an employee":
+          addEmployee();
+          break;
+
+        case "Update employee role":
+          updateRole();
+          break;
+
+        case "View all roles":
+          viewRoles();
+          break;
+
+        case "View all departments":
+          viewDepartments();
+          break;
+
+        case "View all employees":
+          viewEmployees();
+          break;
+      }
+    });
+}
+// function to add a new role
+function addRole() {
+  console.log("Adding a new role");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleTitle",
+        message: "What is the new role called?",
+      },
+      {
+        type: "input",
+        name: "roleDepartment",
+        message: "What department is the new role under?",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the new role's salary?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        `INSERT INTO role (title, department, salary) VALUES ("${answer.roleTitle}", ${roleDepartment}, ${roleSalary});`,
+        function (err, data) {
+            console.log("Role added");
+        }
+      );
+      connection.end();
+    });
 }
